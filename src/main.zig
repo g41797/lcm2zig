8eaa99e9ede45d1721069f5329dc1a88aa5494db
g41797 <g41797@gmail.com>
2025-05-21 10:16:51 +0300

    Create skeleton of the project

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("\n\nSkeleton of generator\n\n", .{});
 
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
@@ -13,7 +13,7 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("\n\n--------------------------- RUN --------------------\n\n\n\n", .{});
 
     try bw.flush(); // Don't forget to flush!
 }
